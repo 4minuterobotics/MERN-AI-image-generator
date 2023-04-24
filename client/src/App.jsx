8e26ac01b5fe4a3da959a95269e991ef81a1bf82
4minuterobotics <lawrence.william86@gmail.com>
2023-04-23 23:47:16 -0400

    added logo and drew to browser tab

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import './App.css';
 
 
 import { logo } from "./assets";
+import { willLogo } from "./assets";
+import { drewLogo } from "./assets";
+
 import { Home, CreatePost } from "./pages";
 const App = () => {
 	return (
 		<BrowserRouter>
 		<ToastContainer position ="bottom-center" limit={1}/>
 			<header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
-				<Link to="/">
+				{/* <Link to="/">
 					<img src={logo} alt="logo" className="w-28 object-contain"></img>
+				</Link> */}
+				{/* <Link to="/">
+					<img src={drewLogo} alt="logo" className="w-16 round-image object-contain"></img>
+				</Link> */}
+				<Link to="/">
+					<img src={willLogo} alt="logo" className="w-16 object-contain"/>
+					<span className="font-bold text-[#222328] text-[25px]">{' '}Drew It</span>
 				</Link>
 				<Link to="/create-post" className="front-inter front-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
 					Create
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,13 @@
 import download from './download.png';
 import logo from './logo.svg';
+import willLogo from'./logo.png';
+import drewLogo from './drew.jpeg'
 import preview from './preview.png';
 
 export {
   download,
   logo,
+  willLogo,
+  drewLogo,
   preview,
 };
